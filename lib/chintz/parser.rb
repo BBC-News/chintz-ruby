90b55321f82e2273f17faa7b273cc5a87200b143
Joseph Wynn <joseph.wynn@bbc.co.uk>
2016-01-04 16:22:46 +0000

    Consistent style

--- lib/chintz/parser.rb
+++ lib/chintz/parser.rb
@@ -12,19 +12,19 @@ module Chintz
       @renderer.template_path = base_path
     end
 
-    def prepare element_names
+    def prepare(element_names)
       element_names = [element_names] unless element_names.kind_of?(Array)
       element_names.each do |element_name|
-        raise ArgumentError, "String or Array of strings" unless element_name.kind_of? String
+        raise ArgumentError, 'String or Array of strings' unless element_name.kind_of? String
         element = load_yaml element_name
-        raise NameError, "Manifest must provide a Name" unless element["name"]
+        raise NameError, 'Manifest must provide a Name' unless element['name']
         return unless element['dependencies']
         resolve_dependencies element['dependencies']
       end
       @deps
     end
 
-    def load_yaml name
+    def load_yaml(name)
       yaml_path = Dir.glob("#{@base_path}/**/#{name}.yaml").first
 
       raise NameError, "Manifest file for #{name} not found in #{@base_path}" if yaml_path.nil?
@@ -32,7 +32,7 @@ module Chintz
       YAML.load_file yaml_path
     end
 
-    def resolve_dependencies deps
+    def resolve_dependencies(deps)
       deps.each do |type, value|
         unless type == 'elements'
           add_dependency type, value
@@ -45,24 +45,20 @@ module Chintz
       end
     end
 
-    def add_dependency type, value
+    def add_dependency(type, value)
       @deps[type] ||= []
       @deps[type].concat value
     end
 
-    def dependency type
+    def dependency(type)
       @deps[type]
     end
 
-    def render element, data = nil
+    def render(element, data = nil)
       unless data
         data = fixture()
       end
-      @renderer.render_file(element, data);
-    end
-
-    def fixture
-      {"stories" => [0,1]}
+      @renderer.render_file(element, data)
     end
 
   end
